1e03b28c4d38aa99996640c547777aba3bce3f25
MateusMoraes05 <playerbeast154@gmail.com>
2022-07-03 00:44:41 -0300

    changes on layout

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,7 @@ import 'animalScreen.dart';
 import 'dogScreen.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -36,7 +36,7 @@ class _MyHomePageState extends State<MyHomePage> {
     return Scaffold(
       resizeToAvoidBottomInset: false,
       body: Padding(
-        padding: const EdgeInsets.all(30.0),
+        padding: const EdgeInsets.all(28.0),
         child: Column(
           children: [
             const Padding(
@@ -60,58 +60,93 @@ class _MyHomePageState extends State<MyHomePage> {
                 ),
               ),
             ),
-            const Padding(
-              padding: EdgeInsets.only(bottom: 10.0),
-              child: Text("Pesquisa por raça",
-                  style: TextStyle(
-                      color: Colors.black,
-                      fontWeight: FontWeight.bold,
-                      fontSize: 13)),
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Container(
-                  width: MediaQuery.of(context).size.width * 0.4,
-                  height: 200,
-                  decoration: BoxDecoration(
+            Padding(
+              padding: const EdgeInsets.only(bottom: 16.0),
+              child: Container(
+                width: MediaQuery.of(context).size.width,
+                height: 120,
+                decoration: BoxDecoration(
                     border: Border.all(
                         color: const Color.fromARGB(1000, 65, 123, 90)),
-                    borderRadius: BorderRadius.circular(12),
-                  ),
-                  child: IconButton(
-                      onPressed: () => {
-                            Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                  builder: (context) => dogScreen()),
-                            )
-                          },
-                      icon: Image.asset(
-                        "src/dog--v1.png",
-                      )),
-                ),
-                Container(
-                  width: MediaQuery.of(context).size.width * 0.4,
-                  height: 200,
-                  decoration: BoxDecoration(
-                    border: Border.all(
-                        color: const Color.fromARGB(1000, 65, 123, 90)),
-                    borderRadius: BorderRadius.circular(12),
-                  ),
-                  child: IconButton(
-                    onPressed: () => {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(builder: (context) => animalScreen()),
-                      )
-                    },
-                    icon: Image.asset(
-                      "src/cat--v1.png",
+                    borderRadius: BorderRadius.circular(9.0)),
+                child: Row(
+                  children: [
+                    Container(
+                      decoration: BoxDecoration(
+                          color: Colors.black,
+                          borderRadius: BorderRadius.circular(8.0)),
+                      width: MediaQuery.of(context).size.width * 0.3,
                     ),
-                  ),
+                    Container(
+                      width: MediaQuery.of(context).size.width * 0.7 - 60,
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          const Text(
+                            "Vacinação",
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold, fontSize: 20),
+                          ),
+                          ElevatedButton(
+                            style: ElevatedButton.styleFrom(
+                              // Foreground color
+                              onPrimary:
+                                  Theme.of(context).colorScheme.onPrimary,
+                              // Background color
+                              primary: const Color.fromARGB(1000, 65, 123, 90),
+                            ).copyWith(
+                                elevation: ButtonStyleButton.allOrNull(0.0)),
+                            onPressed: () {},
+                            child: const Text('Veja Mais'),
+                          )
+                        ],
+                      ),
+                    )
+                  ],
                 ),
-              ],
+              ),
+            ),
+            Container(
+              width: MediaQuery.of(context).size.width,
+              height: 120,
+              decoration: BoxDecoration(
+                  border: Border.all(
+                      color: const Color.fromARGB(1000, 65, 123, 90)),
+                  borderRadius: BorderRadius.circular(9.0)),
+              child: Row(
+                children: [
+                  Container(
+                    decoration: BoxDecoration(
+                        color: Colors.black,
+                        borderRadius: BorderRadius.circular(8.0)),
+                    width: MediaQuery.of(context).size.width * 0.3,
+                  ),
+                  Container(
+                    width: MediaQuery.of(context).size.width * 0.7 - 60,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        const Text(
+                          "Castração",
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold, fontSize: 20),
+                        ),
+                        ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                            // Foreground color
+                            onPrimary: Theme.of(context).colorScheme.onPrimary,
+                            // Background color
+                            primary: const Color.fromARGB(1000, 65, 123, 90),
+                          ).copyWith(
+                              elevation: ButtonStyleButton.allOrNull(0.0)),
+                          onPressed: () {},
+                          child: const Text('Veja Mais'),
+                        )
+                      ],
+                    ),
+                  )
+                ],
+              ),
             )
           ],
         ),
